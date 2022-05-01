ddd7a0ad0578cd3177e9a1385458d9f82c924cf2
rhrafiulhaque <rh.rafiul.haque@gmail.com>
2022-05-01 22:46:03 +0600

    Delete update

--- index.js
+++ index.js
@@ -78,6 +78,8 @@ const run= async()=>{
         app.delete('/product/:id',async(req,res)=>{
             const id = req.params.id;
             const query={_id:ObjectId(id)};
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
         })
 
 
