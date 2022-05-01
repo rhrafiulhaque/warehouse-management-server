2bdf685b591351a89030a7d0a9ec3eaee4e232d1
rhrafiulhaque <rh.rafiul.haque@gmail.com>
2022-05-01 18:19:48 +0600

    server config

---
+++ index.js
@@ -0,0 +1,45 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middlewire
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vpofv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run= async()=>{
+    try {
+        await client.connect();
+        const productCollection = client.db('electroMart').collection('products');
+        app.get('/products', async(req,res)=>{
+            const query={};
+        const cursor = productCollection.find(query);
+        const products = await cursor.toArray();
+        res.send(products);
+        });
+
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+app.get('/',(req,res)=>{
+    res.send('Node Surver Running')
+});
+
+app.listen(port,()=>{
+    console.log('Listening to port',port);
+})
